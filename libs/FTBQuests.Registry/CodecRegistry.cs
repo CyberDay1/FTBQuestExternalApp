083b1349efdc584f1e69c1f88be7aea822bd8f31
CyberDay1 <connormb5@gmail.com>
2025-10-16 10:43:57 -0500

    x

---
+++ libs/FTBQuests.Registry/CodecRegistry.cs
@@ -0,0 +1,9 @@
+namespace FTBQuests.Registry;
+
+public static class CodecRegistry
+{
+    public static void RegisterAll()
+    {
+        // Temporary stub until Codex integration restores full registry logic
+    }
+}
---
+++ libs/FTBQuests.Registry/QuestRegistry.cs
@@ -0,0 +1,9 @@
+namespace FTBQuests.Registry;
+
+public static class QuestRegistry
+{
+    public static void RegisterAll()
+    {
+        // Temporary stub until Codex integration restores full registry logic
+    }
+}
