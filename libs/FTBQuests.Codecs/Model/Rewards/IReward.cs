f379774e89852de1d9132c698c9cbf0211416698
CyberDay1 <connormb5@gmail.com>
2025-10-16 23:33:16 -0500

    x

--- libs/FTBQuests.Codecs/Model/Rewards/IReward.cs
+++ libs/FTBQuests.Codecs/Model/Rewards/IReward.cs
@@ -1,6 +1,9 @@
 ﻿using FTBQuests.Core.Model;
-using FTBQuests.Core.Enums;using FTBQuests.Assets;// <copyright file="IReward.cs" company="CyberDay1">// Copyright (c) CyberDay1. All rights reserved.// </copyright>using FTBQuests.Codecs.Enums;namespace FTBQuests.Codecs.Model;public interface IReward : IExtraAware{    RewardType RewardType { get; }    string TypeId { get; }}
 
-
-
-
+namespace FTBQuests.Codecs.Model.Rewards
+{
+    public interface IReward
+    {
+        RewardType Type { get; }
+    }
+}
