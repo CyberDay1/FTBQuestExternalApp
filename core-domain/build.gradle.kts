6ceb6bd2fcfbf27fc17a34d6489aeb41b1b19856
CyberDay1 <connormb5@gmail.com>
2025-10-24 14:37:57 -0500

    x

--- core-domain/build.gradle.kts
+++ core-domain/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation("org.xerial:sqlite-jdbc:$sqliteVersion")
 }
