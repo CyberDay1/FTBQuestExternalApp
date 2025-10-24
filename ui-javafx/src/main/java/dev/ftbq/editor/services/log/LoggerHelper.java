fce8026f69e847b204c3950cf9cf76a797e2eb19
CyberDay1 <126724028+CyberDay1@users.noreply.github.com>
2025-10-24 10:51:06 -0500

    services:logging — add LoggerHelper

---
+++ ui-javafx/src/main/java/dev/ftbq/editor/services/log/LoggerHelper.java
@@ -0,0 +1,9 @@
+package dev.ftbq.editor.services.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class LoggerHelper {
+    private LoggerHelper(){}
+    public static Logger get(Class<?> cls){ return LoggerFactory.getLogger(cls); }
+}
